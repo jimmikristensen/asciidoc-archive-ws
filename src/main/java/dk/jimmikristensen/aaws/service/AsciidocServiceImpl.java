6f05296811afbdfa5fe62b2186a736cf2bde229e
Jimmi Kristensen <jikr@tv2.dk>
2015-03-28 20:46:51 +0100

    adding db scheme

---
+++ src/main/java/dk/jimmikristensen/aaws/service/AsciidocServiceImpl.java
@@ -0,0 +1,12 @@
+package dk.jimmikristensen.aaws.service;
+
+import javax.ws.rs.core.Response;
+
+public class AsciidocServiceImpl implements AsciidocService {
+
+    @Override
+    public Response getSubscription(String apikey) {
+        
+    }
+    
+}
